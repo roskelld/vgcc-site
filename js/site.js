9f79602956fe2f6b561ec550f7fafbbdb669e11f
Enkidoo <enkidoo@ca.enkidoo.local>
2019-07-26 17:42:43 -0600

    Site progress update. Added some basic functionality to the space

--- js/site.js
+++ js/site.js
@@ -1,15 +1,73 @@
 
-import Asteroids        from './asteroids.js';
+import {CountUp} from '/vgcc-site/js/lib/countUp.min.js';
 
 class Site {
     constructor() {
 
         M.AutoInit();
 
-    }
+        // STATS
 
-    init() {
+        // Stat Counters
+        this.stats = {
+            games: 26,
+            platforms: 14,
+            years: 41,
+            counties: 2,
+            generations: 4,
+            naps: 10037
+        };
+
+        this.countersEl = document.querySelectorAll( '.counter ');
+        this.napEl = document.querySelector( '#nap-counter' );
+
+        this.counters = [];
+
+        this.countersEl.forEach( counter => {
+            this.counters.push( new CountUp( counter, counter.textContent, {
+                duration: 10
+            } ) );
+        });
+
+        this.nap = new CountUp( 'nap-counter', 20037, {
+            duration: 79999,
+            startVal: 10024,
+        });
+
+        // instantiate the scrollama
+        const scroller = scrollama();
+
+        // setup the instance, pass callback functions
+        scroller
+          .setup({
+            step: '.step',
+            offset: 1,
+            debug: true
+          })
+          .onStepEnter(response => {
+            // { element, index, direction }
+            this.counters.forEach( c => c.start() );
+          })
+          .onStepExit(response => {
+            // { element, index, direction }
+            // this.counters.forEach( c => c.reset() );
+          });
+
+        // setup resize event
+        window.addEventListener('resize', scroller.resize);
+
+
+        this.nap.start();
+
+        // Section Tracker ScrollSpy
+        this.section_elems = document.querySelectorAll('.scrollspy');
+        this.section_instances = M.ScrollSpy.init( this.section_elems, {
+            activeClass: 'active',
+            scrollOffset: 60,
+        });
+
     }
+    init()  {}
 }
 
 
